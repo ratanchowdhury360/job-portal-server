25849849d270cfef3770dec9302f9596586bd932
Md. Ratan Chowdhury <ratanchowdhury360>
2025-12-23 19:55:57 +0600

    get application count for each job

--- index.js
+++ index.js
@@ -88,6 +88,21 @@ app.get('/jobsByEmailAddress', async (req, res) => {
   const query = { hr_email: email }
 })
 
+ app.get('/jobs/applications', async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+      const jobs = await jobsCollection.find(query).toArray();
+
+      // should use aggregate to have optimum data fetching
+      for (const job of jobs) {
+        const applicationQuery = { jobId: job._id.toString() }
+        const application_count = await applicationsCollection.countDocuments(applicationQuery)
+        job.application_count = application_count;
+      }
+      res.send(jobs);
+
+    })
+
 //get single find
 app.get('/jobs/:id', async (req, res) => {
   try {
